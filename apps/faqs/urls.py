b6742b1bda8b722506be7ebef038c363f7cee262
Daniel Samuels <daniel.samuels1@gmail.com>
2015-11-15 20:48:03 +0000

    Switch to the new URLs.py style. Ref onespacemedia/cms#80.

--- apps/faqs/urls.py
+++ apps/faqs/urls.py
@@ -1,27 +1,11 @@
 """ URLs used by the faqs app """
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 import views
 
-urlpatterns = patterns('',
-                       url(
-                           r"^categories/$",
-                           views.FaqCategoryListView.as_view(),
-                           name="category_list"
-                       ),
-                       url(
-                           r"^categories/(?P<category_title>[\w-]+)/$",
-                           views.FaqCategoryView.as_view(),
-                           name="category"
-                       ),
-                       url(
-                           r"^$",
-                           views.FaqListView.as_view(),
-                           name="faq_list"
-                       ),
-                       url(
-                           r"^(?P<faq_title>[\w-]+)/$",
-                           views.FaqView.as_view(),
-                           name="faq"
-                       ),
-)
+urlpatterns = [
+    url(r"^categories/$", views.FaqCategoryListView.as_view(), name="category_list"),
+    url(r"^categories/(?P<category_title>[\w-]+)/$", views.FaqCategoryView.as_view(), name="category"),
+    url(r"^$", views.FaqListView.as_view(), name="faq_list"),
+    url(r"^(?P<faq_title>[\w-]+)/$", views.FaqView.as_view(), name="faq"),
+]
